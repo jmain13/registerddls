c7cc5b84990863c2a9461710f140159245ed5792
ss031 <salvadorsanchez11014@yahoo.com>
2017-02-21 18:04:13 -0600

    Add files via upload

---
+++ EmployeeDDL.sql
@@ -0,0 +1,23 @@
+CREATE EXTENSION "uuid-ossp";
+
+CREATE TYPE role AS ENUM ('General Manager', 'Shift Manager', 'Cashier');
+
+CREATE TABLE Employee (
+    RecordID uuid NOT NULL,
+    fName character varying(32) NOT NULL DEFAULT(''),
+    lName character varying(32) NOT NULL DEFAULT(''),
+    employeeID int NOT NULL DEFAULT(0),
+    active BOOLEAN NOT NULL DEFAULT(FALSE),
+    position role NOT NULL,
+    Manager character varying(32) NOT NULL DEFAULT(''),
+    password character varying(32) NOT NULL,
+    createdOn timestamp without time zone NOT NULL DEFAULT now(),
+    CONSTRAINT employee_pkey PRIMARY KEY (RecordID)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE INDEX ix_employee_employeeID
+    ON Employee
+    USING btree
+    (employeeID);
