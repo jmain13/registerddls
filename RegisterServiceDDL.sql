45f9f3d5c3f8ca84e1d260f3a7d2848439c17727
Jonathan Main <jhmain356@gmail.com>
2017-04-08 15:10:06 -0500

    Merge sprint 2 solutions

--- RegisterServiceDDL.sql
+++ RegisterServiceDDL.sql
@@ -3,7 +3,9 @@ CREATE EXTENSION "uuid-ossp";
 CREATE TABLE product (
   id uuid NOT NULL,
   lookupcode character varying(32) NOT NULL DEFAULT(''),
-  count int NOT NULL DEFAULT(0),
+  quantity int NOT NULL DEFAULT(0),
+  price numeric(7, 2) NOT NULL DEFAULT(0),
+  active boolean NOT NULL DEFAULT(FALSE),
   createdon timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT product_pkey PRIMARY KEY (id)
 ) WITH (
@@ -35,3 +37,57 @@ INSERT INTO product VALUES (
      , 150
      , current_timestamp
 );
+
+CREATE TABLE employee (
+  id uuid NOT NULL,
+  employeeid character varying(32) NOT NULL DEFAULT(''),
+  firstname character varying(128) NOT NULL DEFAULT(''),
+  lastname character varying(128) NOT NULL DEFAULT(''),
+  password character varying(512) NOT NULL DEFAULT(''),
+  active boolean NOT NULL DEFAULT(FALSE), 
+  classification int NOT NULL DEFAULT(0),
+  managerid uuid NOT NULL,
+  createdon timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT employee_pkey PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE INDEX ix_employee_employeeid
+  ON employee
+  USING hash(employeeid);
+
+
+CREATE TABLE transaction (
+  recordID uuid NOT NULL,
+  cashierID int NOT NULL,
+  totalAmount int NOT NULL,
+  transactionType boolean NOT NULL DEFAULT(TRUE), /*TRUE => Sale, FALSE => Return*/
+  referenceID uuid, /*if it's a Sale, then referenceID is NULL; otherwise it's the original Sale uuid*/
+  createdon timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT transaction_key PRIMARY KEY (recordID)
+) WITH (
+  OIDS=FALSE
+);
+  
+CREATE INDEX ix_transaction_recordID
+  ON transaction
+  USING hash(recordID);
+
+
+CREATE TABLE transactionEntry (
+  entryID uuid NOT NULL,
+  fromTransaction uuid NOT NULL,
+  PLU character varying(32) NOT NULL,
+  amount int NOT NULL DEFAULT(0),
+  soldAt numeric(7, 2) NOT NULL DEFAULT(0),
+  PRIMARY KEY (entryID),
+  FOREIGN KEY (fromTransaction) REFERENCES transaction (recordID)
+) WITH (
+  OIDS=FALSE
+);
+
+
+CREATE INDEX ix_transactionEntry_entryID
+  ON transactionEntry
+  USING hash(entryID);
